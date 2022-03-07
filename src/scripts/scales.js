107d520ea906ad20e7876952ebbe94ddd4873ca3
baptiste-pauletto1 <baptistep.contact@gmail.com>
2022-03-07 11:57:38 -0500

    update(js): Setting up scales

---
+++ src/scripts/scales.js
@@ -0,0 +1,15 @@
+
+export function setXScale(width, data) {
+
+    return xScale
+}
+
+export function setYScale(height, data) {
+
+    return yScale
+}
+
+export function setColorScale(data) {
+
+    return colorScale
+}
